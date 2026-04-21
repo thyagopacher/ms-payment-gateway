8e18be4372ce2f5ce47e6128a940fba08d835eee
thyagopacher <thyago.pacher@gmail.com>
2026-04-21 08:25:27 -0300

    fix NewRelicTransactionMiddleware

--- app/Http/Middleware/NewRelicTransactionMiddleware.php
+++ app/Http/Middleware/NewRelicTransactionMiddleware.php
@@ -16,7 +16,7 @@ class NewRelicTransactionMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         $route = $request->route();
-        $endpointTransaction = $route->uri();
+        $endpointTransaction = $request->path();
         $nameTransaction = $request->method() . $endpointTransaction;
 
         newrelic_name_transaction($nameTransaction);
