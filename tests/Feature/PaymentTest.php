ec1e696bc1f6d9be8997aa6f75ca047b081b588a
Thyago  Henrique Pacher <thyago.pacher@gmail.com>
2026-05-30 18:22:44 -0300

    Atualizar o PaymentTest.php

--- tests/Feature/PaymentTest.php
+++ tests/Feature/PaymentTest.php
@@ -44,12 +44,5 @@ class PaymentTest extends TestCase
         return $jsonContent;
     }
 
-    #[Depends("test_get_payments_return_httpcode")]
-    public function test_get_payments_valid_content(array $content): void
-    {
-        $this->assertEquals(true, $content['success'] === true, 'Response success field is not true');
-        $this->assertEquals(true, is_numeric($content['count']) && $content['count'] >= 0, 'Response count field is not a valid number');
-        $this->assertEquals(true, is_array($content['payments']), 'Response payments field is not an array');
-        $this->assertEquals(true, $content['count'] === count($content['payments']), 'Response payments field does not match count');
-    }
+     
 }
